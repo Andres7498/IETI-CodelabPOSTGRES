1abe93a896888418a9d6e1404f15620b2f018f0d
Andres <rixco1998@hotmail.com>
2023-02-26 22:22:06 -0500

    funciona

---
+++ IETI-Spring-Boot-con-Postgres-master/src/main/java/edu/eci/ieti/postgresql/data/repository/UserRepository.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.eci.ieti.postgresql.data.repository;
+import edu.eci.ieti.postgresql.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
